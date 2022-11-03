a2268bad657eb3e3dfd1c6f02e72554428e831c4
DAM <DAM@203-13>
2022-11-03 15:50:52 +0100

    Hem cambiat les faltes d'ortografia

--- src/main/java/com/mycompany/conversio/Conversio.java
+++ src/main/java/com/mycompany/conversio/Conversio.java
@@ -20,6 +20,6 @@ public class Conversio {
         int valor_euros = sc.nextInt();
                 
         double tipus_de_canvi = 0.87d;
-        System.out.println(valor_euros + " eur0s (€) acatualmentt ewuivalen a" + (valor_euros * tipus_de_canvi) + " lliures esterlines (£)");
+        System.out.println(valor_euros + " eur0s (€) actualment equivalen a" + (valor_euros * tipus_de_canvi) + " lliures esterlines (£)");
     }
 }
